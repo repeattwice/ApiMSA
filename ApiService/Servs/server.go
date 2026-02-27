d8103cc6dbef71afa4fa37039e31a68b1350032e
Zlo <mafioznikboss7@gmail.com>
2026-02-27 19:50:46 +0300

    first

---
+++ ApiService/Servs/server.go
@@ -0,0 +1,43 @@
+package servs
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func GetPort() string { // валидировать порт после ввода и улчшить валидацию (сделать проверку не только на пустоту)
+	PortPtr := flag.String("port", "", "Введите номер порта")
+	flag.Parse()
+	port := *PortPtr
+	port = strings.TrimSpace(port)
+	if port == "" {
+		fmt.Println("Ошибка подключения, порт пуст, введите прот снова, без флага:")
+		scaner := bufio.NewScanner(os.Stdin)
+		scaner.Scan()
+		port = scaner.Text()
+		return port
+	}
+	return port
+}
+
+func Createserver() { // недоделанна
+	router := mux.NewRouter()
+	port := GetPort()
+	router.Path("/CreateAccount").Methods("POST")
+	router.Path("/Avtorizacion").Methods("GET")
+	router.Path("/DeleteAccount").Methods("DELETE")
+
+	router.Path("/ShowAllItems").Methods("GET").Queries("").HandlerFunc()            //надо придумать и записать query параметры
+	router.Path("/CreateBuy").Methods("POST").Queries("").HandlerFunc()              //надо придумать и записать query параметры
+	router.Path("/GetDiliverySrarus").Methods("PATCH").Queries("").HandlerFunc()     //надо придумать и записать query параметры
+	router.Path("/DeleteBuyFromKorzina").Methods("DELETE").Queries("").HandlerFunc() //надо придумать и записать query параметры
+
+	http.ListenAndServe(":"+port, router)
+
+}
---
+++ ApiService/go.mod
@@ -0,0 +1,11 @@
+module Api
+
+go 1.25.0
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.8.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
